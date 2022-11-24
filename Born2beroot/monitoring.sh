1c83339a64f4cd401f39fb6e30191a871b9efb6f
Lucas Henrique Bordonal <lbordona@c1r10s1.42porto.com>
2022-11-24 14:41:06 +0000

    24/11/22 - 14:41

--- Born2beroot/monitoring.sh
+++ Born2beroot/monitoring.sh
@@ -6,7 +6,7 @@
 #    By: lbordona <marvin@42.fr>                    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2022/11/23 13:09:08 by lbordona          #+#    #+#              #
-#    Updated: 2022/11/24 14:38:48 by lbordona         ###   ########.fr        #
+#    Updated: 2022/11/24 14:40:44 by lbordona         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -14,12 +14,13 @@
 
 arch=$(uname -a)
 cpu=$(nproc)
-vcpu=$(
+vcpu=$(cat /proc/cpuinfo | grep processor | wc -l)
 ip=$(hostname -I)
 mac=$(ip a | grep ether | awk '{print $2}')
 
 wall "
 #Architecture: $arch
-#Network: IP $ip ($mac)
 #CPU Physical: $cpu
+#vCPU: $vcpu
+#Network: IP $ip ($mac)
 "
