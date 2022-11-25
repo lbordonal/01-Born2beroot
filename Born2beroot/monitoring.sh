7dec54e8a9e6edaa4c928a2b8a1cf26dd2388c1e
Lucas Henrique Bordonal <lbordona@c1r9s1.42porto.com>
2022-11-25 12:54:11 +0000

    25/11/22 - 12:54

--- Born2beroot/monitoring.sh
+++ Born2beroot/monitoring.sh
@@ -6,15 +6,30 @@
 #    By: lbordona <marvin@42.fr>                    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2022/11/23 13:09:08 by lbordona          #+#    #+#              #
-#    Updated: 2022/11/24 14:40:44 by lbordona         ###   ########.fr        #
+#    Updated: 2022/11/25 12:53:13 by lbordona         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 #!/bin/bash
 
+#Architecture:
 arch=$(uname -a)
+
+#CPU:
 cpu=$(nproc)
 vcpu=$(cat /proc/cpuinfo | grep processor | wc -l)
+
+#RAM:
+total_ram=$(free -m | awk '$1 == "Mem:" {print $2}')
+used_ram=$(free -m | awk '$1 == "Mem:" {print $3}')
+percent_ram=$(free | awk '$1 == "Mem:" {printf("%.2f"), $3/$2*100}')
+
+#DISK:
+total_disk=$(df -h | awk '$1 == "/dev/sda1" {print $2}')
+used_disk=$(df -h | awk '$1 == "/dev/sda1" {print $3}')
+percent_disk=$(df -h | awk '$1 == "/dev/sda1" {print $5}')
+
+#NETWORK:
 ip=$(hostname -I)
 mac=$(ip a | grep ether | awk '{print $2}')
 
@@ -22,5 +37,7 @@ wall "
 #Architecture: $arch
 #CPU Physical: $cpu
 #vCPU: $vcpu
+#Memory Usage: $used_ram/$total_ram MB ($percent_ram)%
+#Disk Usage: $used_disk/$total_disk ($percent_disk)
 #Network: IP $ip ($mac)
 "
