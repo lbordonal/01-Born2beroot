255e600a30d837e56ba21d97b5b54fc190b7f500
Lucas Henrique Bordonal <lbordona@c1r9s1.42porto.com>
2022-11-25 16:50:16 +0000

    25/11/22 - 16:50

--- Born2beroot/monitoring.sh
+++ Born2beroot/monitoring.sh
@@ -6,7 +6,7 @@
 #    By: lbordona <marvin@42.fr>                    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2022/11/23 13:09:08 by lbordona          #+#    #+#              #
-#    Updated: 2022/11/25 12:53:13 by lbordona         ###   ########.fr        #
+#    Updated: 2022/11/25 16:49:55 by lbordona         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -18,6 +18,7 @@ arch=$(uname -a)
 #CPU:
 cpu=$(nproc)
 vcpu=$(cat /proc/cpuinfo | grep processor | wc -l)
+cpu_usage=$(top -bn1 | grep '^%Cpu' | cut -c 9- | xargs | awk '{printf("%.1f%%"), $1 + $3}')
 
 #RAM:
 total_ram=$(free -m | awk '$1 == "Mem:" {print $2}')
@@ -25,9 +26,18 @@ used_ram=$(free -m | awk '$1 == "Mem:" {print $3}')
 percent_ram=$(free | awk '$1 == "Mem:" {printf("%.2f"), $3/$2*100}')
 
 #DISK:
-total_disk=$(df -h | awk '$1 == "/dev/sda1" {print $2}')
-used_disk=$(df -h | awk '$1 == "/dev/sda1" {print $3}')
-percent_disk=$(df -h | awk '$1 == "/dev/sda1" {print $5}')
+total_disk=$(df -Bg | grep '^/dev/' | grep -v '/boot$' | awk '{td += $2} END {print td}')
+used_disk=$(df -Bg | grep '^/dev/' | grep -v '/boot$' | awk '{ud += $3} END {print ud}')
+percent_disk=$(df -Bg | grep '^/dev/' | grep -v '/boot$' | awk '{ud += $3} {td+= $2} END {printf("%d"), (ud/td)*100}')
+
+#LAST BOOT:
+last_boot=$(who -b | awk '{print $3 " " $4}')
+
+#LVM:
+lvm=$(lsblk | grep "lvm" | wc -l)
+
+lvmt=$()
+lvmu=$(if [ $lvmt -eq 0 ]; then echo no; else echo yes; fi)
 
 #NETWORK:
 ip=$(hostname -I)
@@ -37,7 +47,13 @@ wall "
 #Architecture: $arch
 #CPU Physical: $cpu
 #vCPU: $vcpu
-#Memory Usage: $used_ram/$total_ram MB ($percent_ram)%
-#Disk Usage: $used_disk/$total_disk ($percent_disk)
+#Memory Usage: $used_ram/$total_ram MB ($percent_ram%)
+#Disk Usage: $used_disk/$total_disk GB ($percent_disk%)
+#CPU load: $cpu_usage
+#Last boot: $last_boot
+#LVM use:
+#Connections TCP:
+#User log:
 #Network: IP $ip ($mac)
+#Sudo:
 "
